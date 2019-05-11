380ee980cc4cd79583cd5bcedb0bfc1e67191549
Unknown <mike.brits@gmail.com>
2019-05-11 17:11:30 +0100

    WorkerManager Tests

--- src/WorkerManager.test.js
+++ src/WorkerManager.test.js
@@ -14,10 +14,46 @@ describe('Worker Manager', () => {
         const manager = new WorkerManager(5, () => {});
         expect(manager.getAvailableWorkers().length).toBe(6);
 
-        manager.assignStep('B');
-        manager.assignStep('D');
-        manager.assignStep('E');
+    })
+
+    it('Assigns tasks to workers', () => {
+        const manager = new WorkerManager(5, () => {});
+        const workerOne = manager.getAvailableWorkers()[0];
+
+        let a = manager.assignStep('B');
+        let b = manager.assignStep('D');
+        let c = manager.assignStep('E');
+
+        expect(a).toBe(true);
+        expect(b).toBe(true);
+        expect(c).toBe(true);
 
         expect(manager.getAvailableWorkers().length).toBe(3);
+        expect(workerOne.ready).toBe(false);
+        expect(workerOne.currentStep).toBe('B');
+    });
+
+    it('Returns false when there are no available workers to assign', () => {
+        const manager = new WorkerManager(0, () => {});
+        let a = manager.assignStep('B');
+        let b = manager.assignStep('D');
+        expect(a).toBe(true);
+        expect(b).toBe(false);
     })
+
+    it('Passes on a given function for a completed step', () => {
+        let done = false;
+        const flipDone = () => {done = true};
+
+        const manager = new WorkerManager(0, flipDone);
+        manager.assignStep('A');
+
+        expect(done).toBe(false);
+
+        manager.work();
+
+        expect(done).toBe(true);   
+    });
+
+    
 })
